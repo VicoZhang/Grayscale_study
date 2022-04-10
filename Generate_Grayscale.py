994a6d7922a7e5a994412c8e93e7e6215136af36
Vico Zhang <1248130720@qq.com>
2022-04-10 16:31:27 +0800

    load version2

--- Generate_Grayscale.py
+++ Generate_Grayscale.py
@@ -2,7 +2,8 @@
 # Purpose: Generating grayscales form 1D data
 # Author: Vico Zhang
 # Version 1.0 (2022/4/10) Generating the code.
-# ==================================================================
+# Version 2.0 (2022/4/10) Revising and Finalizing.
+# ====================================================================== #
 
 import numpy as np
 import random
@@ -17,8 +18,8 @@ def load_data(filename):
     for line in file:
         line = line.replace("\n", '')
         line = re.split(r"[ ]+", line)
-        if line[0]:
-            data.append(eval(line[0]))
+        if line[data_column]:
+            data.append(eval(line[data_column]))
         else:
             continue
     file.close()
@@ -27,27 +28,27 @@ def load_data(filename):
 
 def turn_grayscale(data):
     lens = len(data)
-    max_start = lens - 4096
+    max_start = lens - dimension_grayscale**2
     starts = []
-    grayscales = []
-    for i in range(500):
+    gray_scales = []
+    for i in range(sampling_value):
         while True:
             start = random.randint(0, max_start)
             if start not in starts:
                 starts.append(start)
                 break
-        temp = data[start: start + 4096]
+        temp = data[start: start + dimension_grayscale**2]
         temp = np.array(temp)
-        gray_temp = temp.reshape(64, 64)
-        grayscales.append(gray_temp)
-    return grayscales
+        gray_temp = temp.reshape(dimension_grayscale, dimension_grayscale)
+        gray_scales.append(gray_temp)
+    return gray_scales
 
 
 def draw_grayscale(graydata):
-    np.savez("graydatas", *graydata)
+    np.savez(name_npz, *graydata)
 
 
-def npz_visionlization(filename):
+def npz_visualization(filename):
     npz_file = np.load(filename, allow_pickle=True)
     for file in npz_file:
         temp = npz_file['{}.npy'.format(file)]
@@ -59,10 +60,23 @@ def npz_visionlization(filename):
     return
 
 
+def divide_dataset(filename):
+    npz_file = np.load(filename, allow_pickle=True)
+    for file in npz_file:
+        temp = npz_file[file]
+        imageio.imwrite("DataSet\\{}.jpg".format(file), temp)
+    return
+
+
 if __name__ == '__main__':
+    dimension_grayscale = 256
+    data_column = 0
+    sampling_value = 500
+    name_npz = 'grayscales'
     DataSet = load_data('data.txt')
     graydata_temp = turn_grayscale(DataSet)
     draw_grayscale(graydata_temp)
-    npz_visionlization('graydatas.npz')
+    # npz_visualization('graydatas.npz')
+    divide_dataset(name_npz + '.npz')
     print('success!')
 
